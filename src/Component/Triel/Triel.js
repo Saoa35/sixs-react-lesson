397bdea12994d2aba0b5a15c691551debea74f97
Saoa35 <saoamah@gmail.com>
2022-05-14 12:04:34 +0200

    create component

---
+++ src/Component/Triel/Triel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Triel extends React.Component {
+
+    render() {
+
+        return(
+            <div>
+              Triels Component  
+            </div>
+        )
+    }
+}
+
+export default Triel;
