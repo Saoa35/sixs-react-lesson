e4338ea526a6c06473c1f6f2b1b923bc64ee964d
Saoa35 <saoamah@gmail.com>
2022-05-18 01:46:23 +0530

    create file @ add components

---
+++ src/Component/StylesCompExamples/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+
+export const Section = styled.section`
+    border: 1px solid black;
+    padding: 15px;
+    margin: 15px;
+`;
+export const Button = styled.button`
+    display: inline-block;
+    border-radius: 3px;
+    padding: 0.5rem 0;
+    margin: 0.5rem 1rem;
+    width: 11rem;
+    background: transparent;
+    color: grey;
+    border: 2px solid grey;
+    cursor: pointer;
+`;
