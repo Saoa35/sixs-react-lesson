10b281f115d3444a8bc5d0ef58e6a62ea14bb757
Saoa35 <saoamah@gmail.com>
2022-05-18 01:51:59 +0530

    +section @ button comp

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import StylesCompExamples from '../StylesCompExamples/StylesCompExamples';
 import Triel from '../Triel/Triel';
 import './App.css';
 
@@ -17,6 +18,13 @@ class App extends React.Component {
     return (
       <div className="App">
         <h2 style={inlineStyles}>Some Text</h2>
+
+        <h3 className='h3-test'>Test text1</h3>
+
+        <Triel/>
+
+        <StylesCompExamples/>
+        
       </div>
     );
   }
