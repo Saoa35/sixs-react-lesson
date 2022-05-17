cc3d313d3a2e4dc640a6a436e9636960ee725e97
Saoa35 <saoamah@gmail.com>
2022-05-18 00:07:22 +0530

    + inline styles

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,12 +1,25 @@
+import React from 'react';
 import Triel from '../Triel/Triel';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Triel/>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+
+    const textAlign = 'center';
+    const color = 'red';
+
+    const inlineStyles = {
+      color,
+      textAlign
+    }
+
+    return (
+      <div className="App">
+        <h2 style={inlineStyles}>Some Text</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
