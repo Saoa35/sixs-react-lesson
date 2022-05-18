3fc010995cb84ad56f5ea3c7186e6ee1584d557e
Saoa35 <saoamah@gmail.com>
2022-05-18 21:47:08 +0200

    + import @ change to modules

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import StylesCompExamples from '../StylesCompExamples/StylesCompExamples';
 import Triel from '../Triel/Triel';
-import './App.css';
+import styles from './App.module.css';
 import clsx from 'clsx';
 
 
 class App extends React.Component {
+
+  state = {
+
+    clicked: false
+
+  } 
 
   render() {
 
@@ -18,8 +24,22 @@ class App extends React.Component {
     }
 
     return (
-      <div className="App">
+      <div className={styles.App}>
         <h2 style={inlineStyles}>Some Text</h2>
+
+        <h4 className={clsx(
+          'h3-test', 
+          this.state.clicked && 'hidden-class', 
+          'test-class')}
+          >
+            Text in h4 headline
+        </h4>
+
+        <p>
+          <button onClick={() => this.setState({clicked: !this.state.clicked})}>
+            Click me
+          </button>
+        </p>
 
         <h3 className='h3-test'>Test text1</h3>
 
